2d9ecdb53a33a4ef45538997d34a6aa61fd59777
MatthiasMatthys <matthias.matthys2@student.howest.be>
2024-04-24 19:16:10 +0200

    Fix solution first puzzle

--- scripts/script.js
+++ scripts/script.js
@@ -13,13 +13,21 @@ function checkInput() {
     var message = "";
     if (userInput.length !== 5) {
         message ="Input must be 5 characters long.";
+        popupTitle = "Answer incorrect."
     }else if(userInput.toUpperCase() != "KOALA"){
         message ="Wrong answer.";
+        popupTitle = "Answer incorrect."
     } else {
-        message = "Good job!"
+        message = "Good job! The answer was indeed 'koala'!"
+        popupTitle = "Answer correct!"
+        locationMessage = "De locatie van je eerste cadeaux kan je vinden in je kamer. Ik zou een ladder gebruiken of je bureaustoel ;)"
+        var locationElement = document.getElementById("gift1location");
+        locationElement.innerHTML = locationMessage;
     }
     var popup = document.getElementById("popup");
+    var title = document.getElementById("popup-title");
     var binaryMessage = document.getElementById("binaryMessage");
+    title.innerHTML = popupTitle;
     binaryMessage.innerHTML = message;
     popup.style.display = "block";
 }
